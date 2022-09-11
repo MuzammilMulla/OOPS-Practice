62218fa01262ec73e400fdcb0f6eb41dabd0d39a
Muzamil N Mulla <muzammilmulla999@gmail.com>
2022-09-11 16:06:23 +0530

    Linked list in java

--- Main.java
+++ Main.java
@@ -1,26 +1,15 @@
-package oops.practice.inheritance;
+package linkedList;
+
+import javax.sound.sampled.Line;
 
 public class Main {
     public static void main(String[] args) {
-        Box box1= new Box(2,4,6);
-        BoxPrint box2 = new BoxPrint(2,4,6, 3);
-        System.out.println(box2.weight); //3
+        LinkedList list = new LinkedList();
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
 
-        BoxPrint box3= new BoxPrint(box1, 10);
-        System.out.println(box3.l + " " + box3.weight);  //2 10
-
-        Box box4 = new BoxPrint(3,5,7,9);
-        box4.sum(); //15
-        //  --> box4.sub();  // error
-        Box box5 = new BoxPrint(3,5,7,9);
-        box4.sum(); //15
-
-
-
-
-
-
-
+        list.show();
     }
-
 }
